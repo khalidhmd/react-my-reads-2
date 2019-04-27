ddb6cede9ebc4b5098ea153156ef2461a683d235
khalidhmd <khalidhmd@outlook.com>
2019-04-27 16:45:59 +0200

    populate book component  dynamically from book data

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,7 @@ import React from "react";
 
 class Book extends React.Component {
   render() {
+    const book = this.props.book;
     return (
       <div className="book">
         <div className="book-top">
@@ -10,12 +11,11 @@ class Book extends React.Component {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+              backgroundImage: `url("${book.imageLinks["thumbnail"]}")`
             }}
           />
           <div className="book-shelf-changer">
-            <select>
+            <select defaultValue={book.shelf}>
               <option value="move" disabled>
                 Move to...
               </option>
@@ -26,8 +26,8 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
     );
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,24 +1,19 @@
 import React from "react";
 import Book from "./Book";
 class BookShelf extends React.Component {
-  componentDidMount() {
-    const shelfBooks = this.props.books.filter(
-      book => book.shelf === this.props.key
-    );
-    console.log(this.props);
-  }
   render() {
+    const books = this.props.books || [];
+    console.log(books);
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-            <li>
-              <Book />
-            </li>
+            {books.map(book => (
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
